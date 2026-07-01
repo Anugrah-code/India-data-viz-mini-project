8e25f422177a9c0e898f6ddd4b8b1fd746cb2d02
Anugrah_Singh <anugrahsingh270@gmail.com>
2026-07-01 17:30:00 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,36 @@
+import streamlit as st
+import numpy as np
+import pandas as pd
+import plotly.express as px
+import dash
+from dash import html,dcc
+from dash.dependencies import Input, Output
+
+st.set_page_config(layout = 'wide')
+
+df = pd.read_csv('india.csv')
+
+list_of_states = list(df['state'].unique())
+list_of_states.insert(0,'Overall India')
+
+st.sidebar.title('India ka Data Viz')
+
+selected_state = st.sidebar.selectbox('Select State', list_of_states)
+primary = st.sidebar.selectbox('Select Primary Parameter',sorted(df.columns[5:]))
+secondary = st.sidebar.selectbox('Select Secondary Parameter',sorted(df.columns[5:]))
+
+plot = st.sidebar.button('Plot graph')
+
+if plot:
+    st.text('Size represent primary parameter')
+    st.text('Size represent secondary parameter')
+    if selected_state == 'Overall India':
+    # Plot For India
+        fig = px.scatter_mapbox(df, lat = 'Latitude', lon = 'Longitude',size = primary,color = secondary, zoom = 4,   size_max = 35, mapbox_styel = 'carto-positron', width = 1200,height = 700, hover_name= 'District')
+        st.plotly_chart(fig)
+else:
+    # Plot For State
+        state  = df[df['State']== selected_state]
+
+        fig = px.scatter_mapbox(state, lat='Latitude', lon='Longitude', size=primary, color=secondary, zoom=6, size_max=35, mapbox_styel='carto-positron', width=1200, height=700, hover_name= 'District')
+        st.plotly_chart(fig)
