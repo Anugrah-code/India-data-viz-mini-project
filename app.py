a9b0b7f8502b79931a12329f8175ef8c32b6f1e8
Anugrah_Singh <anugrahsingh270@gmail.com>
2026-07-01 18:10:54 +0530

    add requirements.txt

--- app.py
+++ app.py
@@ -32,3 +32,6 @@ else:
         state  = df[df['State']== selected_state]
         fig = px.scatter_mapbox(state, lat='Latitude', lon='Longitude', size=primary, color=secondary, zoom=6, size_max=35, mapbox_style='carto-positron', width=1200, height=700, hover_name= 'District')
         st.plotly_chart(fig)
+
+
+
